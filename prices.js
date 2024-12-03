cf4d45a8c1c6f4ac840fa3a641086216b4fa4c8e
ruletz <77472758+ruletz@users.noreply.github.com>
2024-12-03 22:24:05 +0000

    Update prices.js

--- prices.js
+++ prices.js
@@ -1,35 +1,25 @@
-const fetch = require("node-fetch");
-
 export default async function handler(req, res) {
-  const API_KEY = process.env.LCW_API_KEY; // Use environment variable for API key
-  const API_ENDPOINT = "https://api.livecoinwatch.com/coins/map";
-
-  // List of coin codes to fetch
-  const coinCodes = ["BTC", "ETH", "XRP", "SOL", "BNB", "USDC", "DOGE", "ADA", "TRX", "TON", "AVAX", "SHIB", "LINK", "BCH", "DOT", "NEAR", "SUI", "LTC", "UNI", "APT", "ICP", "KAS", "XMR", "RNDR", "ETC", "STX", "HBAR", "AR", "AAVE", "FIL", "OP", "FTM", "VET", "ATOM", "XTZ", "ALGO", "MATIC", "IMX", "CAKE", "PEPE", "BTT", "MKR", "THETA", "HNT", "KCS", "LDO", "WLD", "IOTA", "ZEC", "EOS", "NEO", "LUNA", "GRT", "ZIL", "DASH", "XRD", "DOGS", "TURBO", "GOAT", "AI", "FET", "AGIX", "TCN", "SEI", "OCEAN"];
-
+  const apiKey = process.env.LCW_API_KEY;
+  const endpoint = "https://api.livecoinwatch.com/coins/map";
+  
   try {
-    const response = await fetch(API_ENDPOINT, {
+    const response = await fetch(endpoint, {
       method: "POST",
       headers: {
-        "x-api-key": API_KEY,
+        "x-api-key": apiKey,
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
         currency: "USD",
-        codes: coinCodes,
+        codes: ["BTC", "ETH", "XRP", "SOL", "BNB", "USDC", "DOGE", "ADA", "TRX", "TON", "AVAX", "SHIB", "LINK", "BCH", "DOT", "NEAR", "SUI", "LTC", "UNI", "APT", "ICP", "KAS", "XMR", "RNDR", "ETC", "STX", "HBAR", "AR", "AAVE", "FIL", "OP", "FTM", "VET", "ATOM", "XTZ", "ALGO", "MATIC", "IMX", "CAKE", "PEPE", "BTT", "MKR", "THETA", "HNT", "KCS", "LDO", "WLD", "IOTA", "ZEC", "EOS", "NEO", "LUNA", "GRT", "ZIL", "DASH", "XRD", "DOGS", "TURBO", "GOAT", "AI", "FET", "AGIX", "TCN", "SEI", "OCEAN"], // Include the codes you want
         meta: true,
       }),
     });
 
-    const data = await response.json();
-    const prices = data.map(coin => ({
-      name: coin.code,
-      price: coin.rate,
-    }));
-
-    res.status(200).json(prices);
+    const apiData = await response.json();
+    res.status(200).json(apiData); // Send the fetched data as JSON response
   } catch (error) {
-    console.error("Error fetching prices:", error);
+    console.error("Error fetching data:", error);
     res.status(500).json({ error: "Failed to fetch data" });
   }
 }
